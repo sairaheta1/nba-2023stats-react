20c0890f4c60b47e4f7b1172652fd8fe8576aa65
Samantha Iraheta <sairaheta1@gmail.com>
2023-04-27 18:31:19 -0700

    finsihed resultls component

--- src/components/TeamView/teamview.jsx
+++ src/components/TeamView/teamview.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import * as NBAIcons from 'react-nba-logos'
+import './teamview.scss'
 const Teamview = (teamData) => {
   // want to credit drewjspivey on gitHub for this:
   const Icon = NBAIcons[teamData.team];
   // ^^
   return (
-  <>
-  <Icon></Icon>
-  <h1>{teamData.team}</h1>
-  </>
+  <div className="team-component">
+    <Icon size={60}></Icon>
+    <h1 className="team-name">{teamData.team}</h1>
+  </div>
   );
 };
 
